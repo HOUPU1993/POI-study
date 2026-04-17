17fe9976e62598f0a643e24fa2107795639625b9
HOUPU1993 <128195276+HOUPU1993@users.noreply.github.com>
2026-04-17 15:26:13 -0700

    updates

--- poi_study/__init__.py
+++ poi_study/__init__.py
@@ -1,2 +1,3 @@
-from .analysis import run_mr_analysis
+from .mr_analysis import run_mr_analysis
+from .lc_analysis import run_lc_analysis
 from .plotting import plot_method_A_pooled, plot_method_C_pooled
